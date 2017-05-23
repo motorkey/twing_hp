d2c731dd3690b189b89a820e1b2803ede9a551bf
turkeeeey <tatapo@tataponoMacBook-Pro.local>
2017-05-23 12:46:58 +0900

    jquery animation

---
+++ js/top.js
@@ -0,0 +1,35 @@
+$(function(){
+  $('#news1').mouseover(function() {
+    $('#bar1').animate({'height': '40px'},30);
+  });
+});
+
+$(function(){
+  $('#news1').mouseleave(function() {
+    $('#bar1').animate({'height': '0px'},0);
+  });
+});
+
+$(function(){
+  $('#news2').mouseover(function() {
+    $('#bar2').animate({'height': '40px'},30);
+  });
+});
+
+$(function(){
+  $('#news2').mouseleave(function() {
+    $('#bar2').animate({'height': '0px'},0);
+  });
+});
+
+$(function(){
+  $('#news3').mouseover(function() {
+    $('#bar3').animate({'height': '40px'},30);
+  });
+});
+
+$(function(){
+  $('#news3').mouseleave(function() {
+    $('#bar3').animate({'height': '0px'},0);
+  });
+});
