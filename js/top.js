fd2235e2f744faa47053c216f44e6b14a8f6b337
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2017-06-23 20:10:16 +0900

    indent & contact.jsの修正

--- js/top.js
+++ js/top.js
@@ -69,21 +69,4 @@ console.log($topProjectLeftNegativeMargin);
       $(".hamburger--open").toggle();
       $(".hamburger--close").toggle();
   });
-
-  // contactNav
-  $("#companyNav").hover(()=> {
-      $(".contactForm--student").css("display", "none");
-      $(".contactForm--univ").css("display", "none");
-      $(".contactForm--company").css("display", "block");
-  });
-  $("#studentNav").hover(()=> {
-      $(".contactForm--company").css("display", "none");
-      $(".contactForm--univ").css("display", "none");
-      $(".contactForm--student").css("display", "block");
-  });
-  $("#univNav").hover(()=> {
-      $(".contactForm--student").css("display", "none");
-      $(".contactForm--company").css("display", "none");
-      $(".contactForm--univ").css("display", "block");
-  });
 });
