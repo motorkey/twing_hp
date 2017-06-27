812fa3fbd5f437f67c7d1618152ac3f528ebc325
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2017-06-28 00:23:59 +0900

    google map修正

--- js/about.js
+++ js/about.js
@@ -3,7 +3,18 @@ function initialize() {
     var myOptions = {
         zoom: 18, // 拡大倍率
         center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP // 地図の種類
+        mapTypeId: google.maps.MapTypeId.ROADMAP, // 地図の種類
+        styles: [{
+            featureType: 'all',
+            elementType: 'all',
+            stylers: [{
+                hue: '#0065ff' //16進数のRGB値を記載
+            }, {
+                saturation: -40 //-100〜100の値を記載
+            }, {
+                lightness: -10 //-100〜100の値を記載
+            }]
+        }]
     };
     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); // 地図を表示
     var marker = new google.maps.Marker({
