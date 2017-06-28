d151be961fba832d501dcd7f9f53d6400b3bbb4e
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2017-06-28 15:17:55 +0900

    contact.js修正

--- js/contact.js
+++ js/contact.js
@@ -1,19 +1,3 @@
 $(()=> {
 
-    // contactNav
-    $("#companyNav").hover(()=> {
-        $(".contactForm--student").css("display", "none");
-        $(".contactForm--univ").css("display", "none");
-        $(".contactForm--company").css("display", "block");
-    });
-    $("#studentNav").hover(()=> {
-        $(".contactForm--company").css("display", "none");
-        $(".contactForm--univ").css("display", "none");
-        $(".contactForm--student").css("display", "block");
-    });
-    $("#univNav").hover(()=> {
-        $(".contactForm--student").css("display", "none");
-        $(".contactForm--company").css("display", "none");
-        $(".contactForm--univ").css("display", "block");
-    });
 });
